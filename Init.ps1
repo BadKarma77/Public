30fc100f0e03606b83ed02fe829adb49261fe7e1
BadKarma77 <139016972+BadKarma77@users.noreply.github.com>
2026-04-06 19:46:53 +0200

    Update Init.ps1

--- Init.ps1
+++ Init.ps1
@@ -1,9 +1,12 @@
 New-Item -ItemType Directory -Force -Path C:\Temp
 New-Item -ItemType Directory -Force -Path C:\Install
-Set-LocalUser -Name "l_admin" -AccountNeverExpires
-Set-LocalUser -Name "l_admin" -PasswordNeverExpires $false
-Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "l_admin"
-Remove-LocalGroupMember -Group "FSLogix Profile Include List" -Member "Jeder"
+Invoke-WebRequest 'https://raw.githubusercontent.com/BadKarma77/Public/refs/heads/main/NeverRed.ps1' -OutFile C:\Install/NeverRed.ps1
+<#
+#Set-LocalUser -Name "l_admin" -AccountNeverExpires
+#Set-LocalUser -Name "l_admin" -PasswordNeverExpires $false
+#Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "l_admin"
+#Remove-LocalGroupMember -Group "FSLogix Profile Include List" -Member "Jeder"
+#>
 <#
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
@@ -13,5 +16,5 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 # administrative code here
 #>
 #
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noexit -ExecutionPolicy Bypass -file "\\AZUVM337015\Neverred$\NeverRed.ps1" -ESFile \\AZUVM337015\Neverred$\LastSetting.txt
+#C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noexit -ExecutionPolicy Bypass -file "\\AZUVM337015\Neverred$\NeverRed.ps1" -ESFile \\AZUVM337015\Neverred$\LastSetting.txt
 
