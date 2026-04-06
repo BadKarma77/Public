148b9a1e8282ade11e0ad3fe45f97a134852d7f0
BadKarma77 <139016972+BadKarma77@users.noreply.github.com>
2026-04-06 16:55:07 +0200

    Add files via upload

---
+++ Init.ps1
@@ -0,0 +1,17 @@
+New-Item -ItemType Directory -Force -Path C:\Temp
+New-Item -ItemType Directory -Force -Path C:\Install
+Set-LocalUser -Name "l_admin" -AccountNeverExpires
+Set-LocalUser -Name "l_admin" -PasswordNeverExpires $false
+Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "l_admin"
+Remove-LocalGroupMember -Group "FSLogix Profile Include List" -Member "Jeder"
+<#
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Exit
+}
+#
+# administrative code here
+#>
+#
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noexit -ExecutionPolicy Bypass -file "\\AZUVM337015\Neverred$\NeverRed.ps1" -ESFile \\AZUVM337015\Neverred$\LastSetting.txt
+
