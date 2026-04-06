5f7b47d5aeb55522470b89602354a456d05a55af
BadKarma77 <139016972+BadKarma77@users.noreply.github.com>
2026-04-06 22:49:37 +0200

    Update Init.ps1

--- Init.ps1
+++ Init.ps1
@@ -6,12 +6,12 @@ Invoke-WebRequest 'https://raw.githubusercontent.com/BadKarma77/Public/refs/head
 #
 New-LocalUser -Name "l_admin" -AccountNeverExpires -PasswordNeverExpires $true
 Add-LocalGroupMember -Member "l_admin" -Group "Administrators"
-<#
-#Set-LocalUser -Name "l_admin" -AccountNeverExpires
-#Set-LocalUser -Name "l_admin" -PasswordNeverExpires $false
-#Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "l_admin"
-#Remove-LocalGroupMember -Group "FSLogix Profile Include List" -Member "Jeder"
-#>
+Set-LocalUser -Name "l_admin" -AccountNeverExpires
+Set-LocalUser -Name "l_admin" -PasswordNeverExpires $false
+Add-LocalGroupMember -Group "FSLogix Profile Exclude List" -Member "l_admin"
+Remove-LocalGroupMember -Group "FSLogix Profile Include List" -Member "\Jeder"
+Remove-LocalGroupMember -Group "FSLogix ODFC Include List" -Member "\Jeder"
+
 <#
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
