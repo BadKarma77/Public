e7101cb1fd37fe8e1b79d19161663566885dadd1
BadKarma77 <139016972+BadKarma77@users.noreply.github.com>
2026-04-09 11:26:29 +0200

    Update Init.ps1

--- Init.ps1
+++ Init.ps1
@@ -2,8 +2,10 @@ Write-Host "AIB Customization: Init.ps1 started"
 #
 New-Item -ItemType Directory -Force -Path C:\Temp
 New-Item -ItemType Directory -Force -Path C:\Install
+# download from GitHub
 #Invoke-WebRequest 'https://raw.githubusercontent.com/BadKarma77/Public/refs/heads/main/NeverRed.ps1' -OutFile C:\Install\NeverRed.ps1
-Invoke-WebRequest 'https://storagetmpblob33.blob.core.windows.net/never/NeverRed.ps1' -OutFile C:\Install\NeverRed.ps1
+# download from Azure Blob
+#Invoke-WebRequest 'https://storagetmpblob33.blob.core.windows.net/never/NeverRed.ps1' -OutFile C:\Install\NeverRed.ps1
 #
 #New-LocalUser -Name "l_admin" -AccountNeverExpires -PasswordNeverExpires   # would ask for password
 #Add-LocalGroupMember -Member "l_admin" -Group "Administrators"
@@ -14,6 +16,7 @@ Invoke-WebRequest 'https://storagetmpblob33.blob.core.windows.net/never/NeverRed
 Remove-LocalGroupMember -Group "FSLogix Profile Include List" -Member "\Jeder"
 Remove-LocalGroupMember -Group "FSLogix ODFC Include List" -Member "\Jeder"
 
+Write-Host "AIB Customization: execute NeverRed install script"
 <#
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
@@ -25,6 +28,6 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 #
 #C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noexit -ExecutionPolicy Bypass -file "\\AZUVM337015\Neverred$\NeverRed.ps1" -ESFile \\AZUVM337015\Neverred$\LastSetting.txt
 #
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noexit -ExecutionPolicy Bypass -file "\\33storagecn.file.core.windows.net\profilescn\Neverred\NeverRed.ps1" -ESFile \\33storagecn.file.core.windows.net\profilescn\Neverred\LastSetting.txt
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -noexit -ExecutionPolicy Bypass -file "\\33storagecn.file.core.windows.net\profilescn\NeverRed\NeverRed.ps1" -ESFile \\33storagecn.file.core.windows.net\profilescn\Neverred\LastSetting.txt
 #
 Write-Host "AIB Customization: Init.ps1 finished"
